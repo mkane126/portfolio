0890cde272128b84507ec93c6594c0d033c4f84b
mkane126 <44822337+mkane126@users.noreply.github.com>
2022-06-13 14:48:02 -0400

    Added routing for all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { SkillsPageComponent } from './components/skills-page/skills-page.component';
+import { EducationPageComponent } from './components/education-page/education-page.component';
+import { ExperiencePageComponent } from './components/experience-page/experience-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path: 'home-page', component: HomePageComponent},
-  { path: 'about-page', component: AboutPageComponent}
+  { path: 'about-page', component: AboutPageComponent},
+  { path: 'skills-page', component: SkillsPageComponent},
+  { path: 'education-page', component: EducationPageComponent},
+  { path: 'experience-page', component: ExperiencePageComponent},
+  { path: 'contact-page', component: ContactPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { SkillsPageComponent } from './components/skills-page/skills-page.component';
+import { EducationPageComponent } from './components/education-page/education-page.component';
+import { ExperiencePageComponent } from './components/experience-page/experience-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SkillsPageComponent,
+    EducationPageComponent,
+    ExperiencePageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
