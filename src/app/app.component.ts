5ab9cc7e6f0c3eaeea63a987dfb161ecbd465dbd
mkane126 <44822337+mkane126@users.noreply.github.com>
2022-06-14 13:48:58 -0400

    skills page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { faPython } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mk-portfolio';
+  title: string = 'mk-portfolio';
+  faPython = faPython;
 }
--- src/app/components/skills-page/skills-page.component.ts
+++ src/app/components/skills-page/skills-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPython, faJava, faGitAlt,faAngular, faLinux, faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faA } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-skills-page',
   templateUrl: './skills-page.component.html',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsPageComponent implements OnInit {
 
+  faPython = faPython;
+  faJava = faJava;
+  faGitAlt = faGitAlt;
+  faAngular = faAngular;
+  faLinux = faLinux;
+  faHTML = faHtml5;
+  
   constructor() { }
 
   ngOnInit(): void {
